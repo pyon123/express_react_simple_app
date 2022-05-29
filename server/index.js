8569eddcaae07e39e16ffa596c456df5dccf7b78
pyon123 <pyon123@outlook.com>
2022-05-29 16:18:53 +0800

    install server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT | 3010;
+
+const tempData = [
+  ["AAPL", "Apple Computer"],
+  ["TSLA", "Tesla Inc."],
+  ["PG", "Procter & Gamble Company, The"],
+  ["IBM", "International Business Machines Corporation"],
+  ["APDN", "Applied DNA Sciences, Inc."],
+];
+
+app.get("/", (req, res) => {
+  res.send("version 0.0");
+});
+
+app.get("/data", (req, res) => {
+  const data = tempData
+    .map((item) => ({ symbol: item[0], name: item[1] }))
+    .sort((a, b) => {
+      if (a.symbol > b.symbol) return 1;
+      if (a.symbol < b.symbol) return -1;
+      return 0;
+    });
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
